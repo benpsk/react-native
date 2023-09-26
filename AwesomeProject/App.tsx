7988e70bea2f70f91ecc51f30846a23cb286bc49
Ben Gunn <bengunn.dev@gmail.com>
2023-09-26 22:42:19 +0630

    add android native module

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -1,62 +1,28 @@
 import React from 'react';
-import {SectionList, StyleSheet, Text, View} from 'react-native';
+import {NativeModules, StyleSheet, Button, View} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 22,
   },
-  sectionHeader: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 20,
-    fontWeight: 'bold',
-    backgroundColor: 'rgba(247,247,247,1.0)',
-  },
-  item: {
-    padding: 10,
-    fontSize: 20,
-    height: 44,
-    marginBottom: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: 'green',
-  },
 });
 
+const {CalendarModule} = NativeModules;
+
 const App = () => {
+
+  const onPress = (): void => {
+    console.log('We will invoke the native module here!');
+    CalendarModule.createCalendarEvent('testName', 'testLocation');
+  };
+
   return (
     <View style={styles.container}>
-      <SectionList
-        sections={[
-          {title: 'D', data: ['Devin', 'Dan', 'Dominic']},
-          {
-            title: 'J',
-            data: [
-              'Jackson',
-              'James',
-              'Jillian',
-              'Jimmy',
-              'Joel',
-              'John',
-              'Julie',
-              'a',
-              'b',
-              'c',
-              'd',
-              'e',
-              'f',
-              'g',
-              'h',
-            ],
-          },
-        ]}
-        renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-        renderSectionHeader={({section}) => (
-          <Text style={styles.sectionHeader}>{section.title}</Text>
-        )}
-        keyExtractor={item => `basicListEntry-${item}`}
+      <Button
+        title="Click to invoke your native module!"
+        color="#841584"
+        onPress={onPress}
       />
     </View>
   );
